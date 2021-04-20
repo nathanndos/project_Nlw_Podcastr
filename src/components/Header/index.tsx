4662a6dae6cd9368bf2d3300a6aa39d1a3f52963
Nathan Silva <nathan.osilva1999@hotmail.com>
2021-04-20 10:49:43 -0300

    Sistema de datas com date-fns adicionado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
+import format from 'date-fns/format' // possibilita formatação da date
+import ptBR from 'date-fns/locale/pt-BR'
+
 import styles from './styles.module.scss'
 
 export function Header(){
+  /*const currentDate = new Date().toLocaleDateString
+      Cria uma instancia do objeto date e converte ele para formato Br
+  */
+      const currentDate = format(new Date, 'EEEEEE, d MMMM')
+
+      // documentação https://date-fns.org/docs/Getting-Started
+
   return(
     <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="podcastr"/>
 
       <p> O melhor para você ouvir, sempre</p>
 
-      <span>Sext, 7 Março</span>
+      <span>{currentDate}</span>
 
     </header>
     
