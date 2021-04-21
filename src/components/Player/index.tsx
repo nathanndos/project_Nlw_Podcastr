504ee13b8d7b9ad5789b02cb271991778d09a965
Nathan Silva <nathan.osilva1999@hotmail.com>
2021-04-21 10:59:56 -0300

    Inserindo bottoes e estilos

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss'
+
+export function Player(){
+
+  return(
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="tocando agora"/>
+        <strong>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+         <span>00:00</span> 
+        </div>
+
+        <div className={styles.Slider}>
+          <div className={styles.emptySlider}/>
+          <span>00:00</span> 
+        </div>
+
+        <div className={styles.buttons}>
+          <button type="button">
+             <img src="/shuffle.svg" alt="Embaralhar"/>
+          </button>
+          <button type="button">
+             <img src="/play-previous.svg" alt="Tocar anterior"/>
+          </button>
+          <button type="button">
+             <img src="/play.svg" alt="Tocar"/>
+          </button>
+          <button type="32button">
+             <img src="/play-next.svg" alt="Tocar próxima"/>
+          </button>
+          <button type="button">
+             <img src="/repeat.svg" alt="Repetir"/>
+          </button>
+
+        </div>
+      </footer>
+      
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
 
+import styles from '../styles/app.module.scss'
+
+function MyApp({ Component, pageProps }) {//O app é executada em todas as chamadas realiadas
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  )
+}
+//Por isso o Header fica aqui, pois em todas as abas o header estará presete
 export default MyApp
