77a07d477d535babdc359c4cab02b69a143f79b7
Nathan Silva <nathan.osilva1999@hotmail.com>
2021-04-28 23:35:48 -0300

    Atualizando

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -65,12 +65,28 @@ export default function Episode({episode}: EpisodeProps){
 }
 
 export const getStaticPaths: GetStaticPaths = async () =>{
+
+  const { data } = await api.get('episodes',{
+    params:{
+      _limit:2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  const paths = data.map(episode =>{
+    return {
+      params:{
+        slug: episode.id
+      }
+    }
+  })
   return{
-    paths:[],
+    paths,
     fallback: 'blocking'
     
   }
 }
+
 export const getStaticProps: GetStaticProps = async (ctx) =>{
 
   const { slug } = ctx.params;
